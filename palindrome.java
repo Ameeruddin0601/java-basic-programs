a18f54fcbd2334e6200eef3a20382978e129f87e
Ameeruddin06 <ameerb0601@gmail.com>
2023-01-24 22:45:52 +0530

    Update

--- palindrome.java
+++ palindrome.java
@@ -2,6 +2,19 @@ class palindrome{
     public static void main(String arg[]){
         int rem,sum=0,temp;
         int n=454; //check if palindrome
+
+        temp=n;
+        while(n>0){
+            rem=n%10;
+            sum = (sum*10) + rem;
+            n=n/10;
+        }
+        if(temp==sum){
+        System.out.println(temp+" is a palindrome Number");
+        }else{
+        System.out.println(temp+" is not a palindrome");
+        }
         
     }
+    
 }
